c2e580cf746a6c88b4a459d7d40aa82ee84b25bd
ProgramX <your.email@mai.ru>
2025-09-30 21:04:39 +0300

    Добавлена sqlalchemy[asyncio]

--- auth_service/main.py
+++ auth_service/main.py
@@ -1,10 +1,19 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
 from app.api.auth import router as auth_router
+from app.models import db_helper, Base
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router)
- 
-if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
